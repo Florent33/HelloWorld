ba44b78c4c8b73ffb37a9aeb0de6dfdf7b781349
Administrateur <administrateur@macbook-pro-de-administrateur.local>
2015-12-23 22:32:53 +0100

    Add space in comments

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -1,10 +1,11 @@
-//
 //  ViewController.swift
 //  HelloWorld
+//  
+//  Create a simple button Hello World with a popup with text : 'Hello World'
 //
-//  Created by Administrateur on 23/12/2015.
+//  Created by Florent FERRAND on 23/12/2015.
 //  Copyright © 2015 com.epsi.helloworld. All rights reserved.
-//
+
 
 import UIKit
 
@@ -20,6 +21,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to my first App", message: "Hello World",
+            preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
 
 }
 
